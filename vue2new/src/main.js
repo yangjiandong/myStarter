9e26b609abaf29583819e81410b402c6143193ee
yangjiandong <59338947>
2016-12-07 17:35:05 +0800

    add multi page

--- vue2new/src/main.js
+++ vue2new/src/main.js
@@ -1,5 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+import VueResource from 'vue-resource'
+
+Vue.config.debug = true;
+Vue.use(VueRouter);
+Vue.use(VueResource);
+
+//
+const First = {template: '<div><h2> My is First</h2></div>'};
 
 new Vue({
   el: '#app',
--- vue2new/webpack.config.js
+++ vue2new/webpack.config.js
@@ -33,7 +33,8 @@ module.exports = {
   },
   resolve: {
     alias: {
-      'vue$': 'vue/dist/vue.common.js'
+      //'vue$': 'vue/dist/vue.common.js'
+        'vue': 'vue/dist/vue.js'
     }
   },
   devServer: {

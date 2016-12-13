ca8ae8d8840581ea8f1e7201239c72eb39427c23
yangjiandong <59338947>
2016-12-13 17:05:06 +0800

    x

--- element/src/App.vue
+++ element/src/App.vue
@@ -3,6 +3,18 @@
     <img src="./assets/logo.png">
     <h1>{{ msg }}</h1>
     <el-button @click.native="startHacking">Let's do it</el-button>
+    <el-button @click="dialogVisible = true">click show dialog</el-button>
+
+    <el-dialog title="show" v-model="dialogVisible" size="tiny">
+      <span>this is a message</span>
+      <sapn slot="footer" class="dialog-footer">
+        <el-button @click="dialogVisible=false">取消</el-button>
+        <el-button type="primary" @click="dialogVisible = false">ok</el-button>
+      </span>
+    </el-dialog>
+
+    <el-button @>
+
   </div>
 </template>
 
@@ -10,7 +22,8 @@
 export default {
   data () {
     return {
-      msg: 'Use Vue 2.0 Today!'
+      msg: 'Use Vue 2.0 Today!',
+      dialogVisible: false
     }
   },
   methods: {
